a706fab8965cceabc72690978cd86098f22c07b1
samedyhunx <samedy.hunx@gmail.com>
2025-10-13 15:31:35 +0700

    feature/sidebar: worked on desktop and mobile sidebar

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,14 +1,41 @@
 "use client";
 
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import { useTranslations } from "next-intl";
+import { AppSidebarClient } from "./_AppSidebarClient";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
 
   return (
-    <div>
-      <h1>{t("title")}</h1>
-      <p>{t("description")}</p>
-    </div>
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span>SamedyX Jobs</span>
+          </SidebarHeader>
+          <SidebarContent>fhjkskhjfs</SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>jdksjfksjk</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">jfljdjsflks</main>
+      </AppSidebarClient>
+    </SidebarProvider>
   );
 }
