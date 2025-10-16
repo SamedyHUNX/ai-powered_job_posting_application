f4a57a4acf91919e6289f0fff1bebe28b54f2ceb
samedyhunx <samedy.hunx@gmail.com>
2025-10-16 23:52:10 +0700

    feature/auth: auth dto

---
+++ backend/src/auth/dtos/auth.dto.ts
@@ -0,0 +1,24 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class SignUpDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  imageUrl: string;
+}
+
+export class SignInDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
