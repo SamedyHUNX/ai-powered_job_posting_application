c4a64960c06973727f29e61cec1050eb1af0e5e5
samedyhunx <samedy.hunx@gmail.com>
2025-10-16 23:56:07 +0700

    feature/auth: working on auth service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { SignUpDto } from "./dtos/auth.dto";
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { SignUpDto } from './dtos/auth.dto';
+import { DrizzleService } from 'src/drizzle/drizzle.service';
+import { UserTable } from 'src/drizzle/schema';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class AuthService {
-    constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private dbService: DrizzleService,
+  ) {}
 
-    async singUp(dto: SignUpDto) {
-        // Check if user exists
-        const existingUser = await db
-    }
+  async singUp(dto: SignUpDto) {
+    // Check if user exists
+    const existingUser = await this.dbService.db
+      .select()
+      .from(UserTable)
+      .where(eq(UserTable.email, dto.email))
+      .limit(1);
+  }
 }
