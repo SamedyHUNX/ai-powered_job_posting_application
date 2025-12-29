c5321daee2bf67b5ed5b01fa8efe7783883cf2cc
samedyhunx <samedy.hunx@gmail.com>
2025-12-29 15:01:11 +0700

    feature/auth: strutured error

--- frontend/src/lib/error-handler.ts
+++ frontend/src/lib/error-handler.ts
@@ -1,15 +1,27 @@
+import axios, { AxiosError } from "axios";
 import { useTranslations } from "next-intl";
-import { ApiError } from "./api-error";
+
+interface ApiError {
+  code?: number;
+  message?: string;
+}
 
 export function useErrorHandler() {
   const t = useTranslations();
 
-  const getErrorMessage = (error: ApiError) => {
-    const errorCode = error.code || 9999;
+  const getErrorMessage = (error: unknown) => {
+    let apiError: ApiError = { code: 9999 };
 
-    return t(`apiErrors.${errorCode}`, {
-      defaultValue: error.code || t(`apiErrors.9999`),
-    });
+    if (axios.isAxiosError(error)) {
+      if (error.response?.data?.code) {
+        apiError.code = error.response.data.code;
+        apiError.message = error.response.data.message;
+      }
+    }
+
+    console.log("normalized error", apiError);
+
+    return t(`apiErrors.${apiError.code}`);
   };
 
   return { getErrorMessage };
